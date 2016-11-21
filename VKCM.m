ed1f3f1ef81abb76e95fcd7b772ea883f997265b
DanielMaida <dfm2@cin.ufpe.br>
2016-11-21 12:59:05 -0300

    gaussian kernel fixed

--- VKCM.m
+++ VKCM.m
@@ -1,13 +1,22 @@
 % INPUT = data set
-data_set = load abalone.data; %check if this is right, probabably not
+load ("data.mat"); %check if this is right, probabably notd
 % INPUT = number of clusters
 number_of_clusters = 3;
 % INPUT = parameter B
 betaparameter = 100; %check this later
 
-function k = gaussianKernel(x,y)
-	k = (1/sqrt(2*pi)*y) * e^(-(pow2(x)/2*pow2(y)));
+function k = gaussianKernel(x,y,sigma)
+    k = e^(-(pow2(mod(x-y))/2*pow2(sigma));
 endfunction
+
+function k = gaussianKernelProduct(x,y,sigma)
+    k = gaussianKernel(x,y,sigma)*x;
+endfunction
+
+function r = centroidCalculator(attribute,centroid)
+    
+endfunction
+ 
 
 % OUTPUT = vector of relevant weights of the variables lambda
 % OUTPUT = partition P of the data set X into K clusters
